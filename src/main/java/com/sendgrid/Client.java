9fc488e2ef0fae7d89e617906d7748f29f5fceec
Marcos Barbero <marcos.hgb@gmail.com>
2016-10-06 14:00:16 -0300

    Refactor: Cleaning code

--- src/main/java/com/sendgrid/Client.java
+++ src/main/java/com/sendgrid/Client.java
@@ -1,43 +1,39 @@
 package com.sendgrid;
 
+import org.apache.http.Header;
+import org.apache.http.HttpHeaders;
+import org.apache.http.StatusLine;
+import org.apache.http.annotation.NotThreadSafe;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.*;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
-
-import org.apache.http.Header;
-import org.apache.http.StatusLine;
-import org.apache.http.annotation.NotThreadSafe;
-import org.apache.http.client.ResponseHandler;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpEntityEnclosingRequestBase;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPatch;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.methods.HttpPut;
-import org.apache.http.client.methods.HttpRequestBase;
-import org.apache.http.client.utils.URIBuilder;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
 
 // Hack to get DELETE to accept a request body
 @NotThreadSafe
 class HttpDeleteWithBody extends HttpEntityEnclosingRequestBase {
-	public static final String METHOD_NAME = "DELETE";
 
-	public String getMethod() {
-		return METHOD_NAME;
-	}
+    public HttpDeleteWithBody(final String uri) {
+        super();
+        setURI(URI.create(uri));
+    }
 
-	public HttpDeleteWithBody(final String uri) {
-		super();
-		setURI(URI.create(uri));
-	}
+    public String getMethod() {
+        return Method.DELETE.name();
+    }
 }
 
 /**
@@ -45,285 +41,234 @@ class HttpDeleteWithBody extends HttpEntityEnclosingRequestBase {
  */
 public class Client {
 
-	private CloseableHttpClient httpClient;
-	private Boolean test;
+    private CloseableHttpClient httpClient;
+    private Boolean test;
 
-	/**
-	 * Constructor for using the default CloseableHttpClient.
-	 */
-	public Client() {
-		this.httpClient = HttpClients.createDefault();
-		this.test = false;
-	}
+    /**
+     * Constructor for using the default CloseableHttpClient.
+     */
+    public Client() {
+        this.httpClient = HttpClients.createDefault();
+        this.test = false;
+    }
 
-	/**
-	 * Constructor for passing in an httpClient for mocking.
-	 *
-	 * @param httpClient
-	 *            an Apache CloseableHttpClient
-	 */
-	public Client(CloseableHttpClient httpClient) {
-		this.httpClient = httpClient;
-		this.test = false;
-	}
+    /**
+     * Constructor for passing in an httpClient for mocking.
+     *
+     * @param httpClient an Apache CloseableHttpClient
+     */
+    public Client(CloseableHttpClient httpClient) {
+        this.httpClient = httpClient;
+        this.test = false;
+    }
 
-	/**
-	 * Constructor for passing in a test parameter to allow for http calls
-	 *
-	 * @param test
-	 *            is a Bool
-	 */
-	public Client(Boolean test) {
-		this.httpClient = HttpClients.createDefault();
-		this.test = test;
-	}
+    /**
+     * Constructor for passing in a test parameter to allow for http calls
+     *
+     * @param test is a Bool
+     */
+    public Client(Boolean test) {
+        this.httpClient = HttpClients.createDefault();
+        this.test = test;
+    }
 
-	/**
-	 * Add query parameters to a URL.
-	 *
-	 * @param baseUri
-	 *            (e.g. "api.sendgrid.com")
-	 * @param endpoint
-	 *            (e.g. "/your/endpoint/path")
-	 * @param queryParams
-	 *            map of key, values representing the query parameters
-	 */
-	public URI buildUri(String baseUri, String endpoint, Map<String, String> queryParams) throws URISyntaxException {
-		URIBuilder builder = new URIBuilder();
-		URI uri;
+    /**
+     * Add query parameters to a URL.
+     *
+     * @param baseUri     (e.g. "api.sendgrid.com")
+     * @param endpoint    (e.g. "/your/endpoint/path")
+     * @param queryParams map of key, values representing the query parameters
+     */
+    public URI buildUri(String baseUri, String endpoint, Map<String, String> queryParams) throws URISyntaxException {
+        URIBuilder builder = new URIBuilder();
 
-		if (this.test == true) {
-			builder.setScheme("http");
-		} else {
-			builder.setScheme("https");
-		}
+        if (this.test) {
+            builder.setScheme("http");
+        } else {
+            builder.setScheme("https");
+        }
 
-		builder.setHost(baseUri);
-		builder.setPath(endpoint);
+        builder.setHost(baseUri);
+        builder.setPath(endpoint);
 
-		if (queryParams != null) {
-			for (Map.Entry<String, String> entry : queryParams.entrySet()) {
-				builder.setParameter(entry.getKey(), entry.getValue());
-			}
-		}
+        if (queryParams != null) {
+            for (Map.Entry<String, String> entry : queryParams.entrySet()) {
+                builder.setParameter(entry.getKey(), entry.getValue());
+            }
+        }
 
-		try {
-			uri = builder.build();
-		} catch (URISyntaxException ex) {
-			throw ex;
-		}
+        return builder.build();
+    }
 
-		return uri;
-	}
+    /**
+     * Prepare a Response object from an API call via Apache's HTTP client.
+     *
+     * @param response from a call to a CloseableHttpClient
+     */
+    public Response getResponse(CloseableHttpResponse response) throws IOException {
+        ResponseHandler<String> handler = new SendGridResponseHandler();
 
-	/**
-	 * Prepare a Response object from an API call via Apache's HTTP client.
-	 *
-	 * @param response
-	 *            from a call to a CloseableHttpClient
-	 */
-	public Response getResponse(CloseableHttpResponse response) throws IOException {
-		ResponseHandler<String> handler = new SendGridResponseHandler();
-		String responseBody = "";
+        int statusCode = response.getStatusLine().getStatusCode();
 
-		int statusCode = response.getStatusLine().getStatusCode();
+        String responseBody = handler.handleResponse(response);
 
-		responseBody = handler.handleResponse(response);
+        Header[] headers = response.getAllHeaders();
+        Map<String, String> responseHeaders = new HashMap<String, String>();
+        for (Header h : headers) {
+            responseHeaders.put(h.getName(), h.getValue());
+        }
 
-		Header[] headers = response.getAllHeaders();
-		Map<String, String> responseHeaders = new HashMap<String, String>();
-		for (Header h : headers) {
-			responseHeaders.put(h.getName(), h.getValue());
-		}
+        return new Response(statusCode, responseBody, responseHeaders);
+    }
 
-		return new Response(statusCode, responseBody, responseHeaders);
-	}
+    /**
+     * Make a GET request and provide the status code, response body and
+     * response headers.
+     */
+    public Response get(Request request) throws URISyntaxException, IOException {
+        URI uri = buildUri(request.baseUri, request.endpoint, request.queryParams);
+        HttpGet httpGet = new HttpGet(uri.toString());
 
-	/**
-	 * Make a GET request and provide the status code, response body and
-	 * response headers.
-	 */
-	public Response get(Request request) throws URISyntaxException, IOException {
-		URI uri = null;
-		HttpGet httpGet = null;
+        if (request.headers != null) {
+            for (Map.Entry<String, String> entry : request.headers.entrySet()) {
+                httpGet.setHeader(entry.getKey(), entry.getValue());
+            }
+        }
+        return executeApiCall(httpGet);
+    }
 
-		try {
-			uri = buildUri(request.baseUri, request.endpoint, request.queryParams);
-			httpGet = new HttpGet(uri.toString());
-		} catch (URISyntaxException ex) {
-			throw ex;
-		}
+    /**
+     * Make a POST request and provide the status code, response body and
+     * response headers.
+     */
+    public Response post(Request request) throws URISyntaxException, IOException {
+        URI uri = buildUri(request.baseUri, request.endpoint, request.queryParams);
+        HttpPost httpPost = new HttpPost(uri.toString());
 
-		if (request.headers != null) {
-			for (Map.Entry<String, String> entry : request.headers.entrySet()) {
-				httpGet.setHeader(entry.getKey(), entry.getValue());
-			}
-		}
-		return executeApiCall(httpGet);
-	}
+        if (request.headers != null) {
+            for (Map.Entry<String, String> entry : request.headers.entrySet()) {
+                httpPost.setHeader(entry.getKey(), entry.getValue());
+            }
+        }
 
-	/**
-	 * Make a POST request and provide the status code, response body and
-	 * response headers.
-	 */
-	public Response post(Request request) throws URISyntaxException, IOException {
-		URI uri = null;
-		HttpPost httpPost = null;
+        httpPost.setEntity(new StringEntity(request.body, Charset.forName(StandardCharsets.UTF_8.toString())));
+        if (!request.body.equals("")) {
+            httpPost.setHeader(HttpHeaders.CONTENT_TYPE, ContentType.APPLICATION_JSON.getMimeType());
+        }
 
-		try {
-			uri = buildUri(request.baseUri, request.endpoint, request.queryParams);
-			httpPost = new HttpPost(uri.toString());
-		} catch (URISyntaxException ex) {
-			throw ex;
-		}
+        return executeApiCall(httpPost);
+    }
 
-		if (request.headers != null) {
-			for (Map.Entry<String, String> entry : request.headers.entrySet()) {
-				httpPost.setHeader(entry.getKey(), entry.getValue());
-			}
-		}
+    /**
+     * Make a PATCH request and provide the status code, response body and
+     * response headers.
+     */
+    public Response patch(Request request) throws URISyntaxException, IOException {
+        URI uri = buildUri(request.baseUri, request.endpoint, request.queryParams);
+        HttpPatch httpPatch = new HttpPatch(uri.toString());
 
-		httpPost.setEntity(new StringEntity(request.body, Charset.forName("UTF-8")));
-		if (request.body != "") {
-			httpPost.setHeader("Content-Type", "application/json");
-		}
+        if (request.headers != null) {
+            for (Map.Entry<String, String> entry : request.headers.entrySet()) {
+                httpPatch.setHeader(entry.getKey(), entry.getValue());
+            }
+        }
 
-		return executeApiCall(httpPost);
-	}
+        httpPatch.setEntity(new StringEntity(request.body, Charset.forName(StandardCharsets.UTF_8.toString())));
+        if (!request.body.equals("")) {
+            httpPatch.setHeader(HttpHeaders.CONTENT_TYPE, ContentType.APPLICATION_JSON.getMimeType());
+        }
+        return executeApiCall(httpPatch);
+    }
 
-	/**
-	 * Make a PATCH request and provide the status code, response body and
-	 * response headers.
-	 */
-	public Response patch(Request request) throws URISyntaxException, IOException {
-		URI uri = null;
-		HttpPatch httpPatch = null;
+    /**
+     * Make a PUT request and provide the status code, response body and
+     * response headers.
+     */
+    public Response put(Request request) throws URISyntaxException, IOException {
+        URI uri = buildUri(request.baseUri, request.endpoint, request.queryParams);
+        HttpPut httpPut = new HttpPut(uri.toString());
 
-		try {
-			uri = buildUri(request.baseUri, request.endpoint, request.queryParams);
-			httpPatch = new HttpPatch(uri.toString());
-		} catch (URISyntaxException ex) {
-			throw ex;
-		}
+        if (request.headers != null) {
+            for (Map.Entry<String, String> entry : request.headers.entrySet()) {
+                httpPut.setHeader(entry.getKey(), entry.getValue());
+            }
+        }
 
-		if (request.headers != null) {
-			for (Map.Entry<String, String> entry : request.headers.entrySet()) {
-				httpPatch.setHeader(entry.getKey(), entry.getValue());
-			}
-		}
+        httpPut.setEntity(new StringEntity(request.body, Charset.forName(StandardCharsets.UTF_8.toString())));
+        if (!request.body.equals("")) {
+            httpPut.setHeader(HttpHeaders.CONTENT_TYPE, ContentType.APPLICATION_JSON.getMimeType());
+        }
 
-		httpPatch.setEntity(new StringEntity(request.body, Charset.forName("UTF-8")));
-		if (request.body != "") {
-			httpPatch.setHeader("Content-Type", "application/json");
-		}
-		return executeApiCall(httpPatch);
-	}
+        return executeApiCall(httpPut);
+    }
 
-	/**
-	 * Make a PUT request and provide the status code, response body and
-	 * response headers.
-	 */
-	public Response put(Request request) throws URISyntaxException, IOException {
-		URI uri = null;
-		HttpPut httpPut = null;
+    /**
+     * Make a DELETE request and provide the status code and response headers.
+     */
+    public Response delete(Request request) throws URISyntaxException, IOException {
+        URI uri = buildUri(request.baseUri, request.endpoint, request.queryParams);
+        HttpDeleteWithBody httpDelete = new HttpDeleteWithBody(uri.toString());
 
-		try {
-			uri = buildUri(request.baseUri, request.endpoint, request.queryParams);
-			httpPut = new HttpPut(uri.toString());
-		} catch (URISyntaxException ex) {
-			throw ex;
-		}
+        if (request.headers != null) {
+            for (Map.Entry<String, String> entry : request.headers.entrySet()) {
+                httpDelete.setHeader(entry.getKey(), entry.getValue());
+            }
+        }
 
-		if (request.headers != null) {
-			for (Map.Entry<String, String> entry : request.headers.entrySet()) {
-				httpPut.setHeader(entry.getKey(), entry.getValue());
-			}
-		}
+        httpDelete.setEntity(new StringEntity(request.body, Charset.forName(StandardCharsets.UTF_8.toString())));
+        if (!request.body.equals("")) {
+            httpDelete.setHeader(HttpHeaders.CONTENT_TYPE, ContentType.APPLICATION_JSON.getMimeType());
+        }
 
-		httpPut.setEntity(new StringEntity(request.body, Charset.forName("UTF-8")));
-		if (request.body != "") {
-			httpPut.setHeader("Content-Type", "application/json");
-		}
+        return executeApiCall(httpDelete);
+    }
 
-		return executeApiCall(httpPut);
-	}
+    private Response executeApiCall(HttpRequestBase httpPost) throws IOException {
+        CloseableHttpResponse serverResponse = null;
+        Response response = new Response();
+        try {
+            serverResponse = httpClient.execute(httpPost);
+            response = getResponse(serverResponse);
+            final StatusLine statusLine = serverResponse.getStatusLine();
+            if (statusLine.getStatusCode() >= 300) {
+                //throwing IOException here to not break API behavior.
+                throw new IOException("Request returned status Code " + statusLine.getStatusCode() + "Body:" + (response != null ? response.body : null));
+            }
 
-	/**
-	 * Make a DELETE request and provide the status code and response headers.
-	 */
-	public Response delete(Request request) throws URISyntaxException, IOException {
-		URI uri = null;
-		HttpDeleteWithBody httpDelete = null;
+        } finally {
+            if (serverResponse != null) {
+                serverResponse.close();
+            }
+        }
+        return response;
+    }
 
-		try {
-			uri = buildUri(request.baseUri, request.endpoint, request.queryParams);
-			httpDelete = new HttpDeleteWithBody(uri.toString());
-		} catch (URISyntaxException ex) {
-			throw ex;
-		}
-
-		if (request.headers != null) {
-			for (Map.Entry<String, String> entry : request.headers.entrySet()) {
-				httpDelete.setHeader(entry.getKey(), entry.getValue());
-			}
-		}
-
-		httpDelete.setEntity(new StringEntity(request.body, Charset.forName("UTF-8")));
-		if (request.body != "") {
-			httpDelete.setHeader("Content-Type", "application/json");
-		}
-
-		return executeApiCall(httpDelete);
-	}
-
-	private Response executeApiCall(HttpRequestBase httpPost) throws IOException {
-		CloseableHttpResponse serverResponse = null;
-		Response response = new Response();
-		try {
-			serverResponse = httpClient.execute(httpPost);
-			response = getResponse(serverResponse);
-			final StatusLine statusLine = serverResponse.getStatusLine();
-			if(statusLine.getStatusCode()>=300){
-				//throwing IOException here to not break API behavior.
-				throw new IOException("Request returned status Code "+statusLine.getStatusCode()+"Body:"+(response!=null?response.body:null));
-			}
-	
-		} finally {
-			if (serverResponse != null) {
-				serverResponse.close();
-			}
-		}
-		return response;
-	}
-
-	/**
-	 * A thin wrapper around the HTTP methods.
-	 */
-	public Response api(Request request) throws IOException {
-		try {
-			if (request.method == null) {
-				throw new IOException("We only support GET, PUT, PATCH, POST and DELETE.");
-			}
-			switch (request.method) {
-			case GET:
-				return get(request);
-			case POST:
-				return post(request);
-			case PUT:
-				return put(request);
-			case PATCH:
-				return patch(request);
-			case DELETE:
-				return delete(request);
-			default:
-				throw new IOException("We only support GET, PUT, PATCH, POST and DELETE.");
-			}
-		} catch (IOException ex) {
-			throw ex;
-		} catch (URISyntaxException ex) {
-			StringWriter errors = new StringWriter();
-			ex.printStackTrace(new PrintWriter(errors));
-			throw new IOException(errors.toString());
-		}
-	}
+    /**
+     * A thin wrapper around the HTTP methods.
+     */
+    public Response api(Request request) throws IOException {
+        try {
+            if (request.method == null) {
+                throw new IOException("We only support GET, PUT, PATCH, POST and DELETE.");
+            }
+            switch (request.method) {
+                case GET:
+                    return get(request);
+                case POST:
+                    return post(request);
+                case PUT:
+                    return put(request);
+                case PATCH:
+                    return patch(request);
+                case DELETE:
+                    return delete(request);
+                default:
+                    throw new IOException("We only support GET, PUT, PATCH, POST and DELETE.");
+            }
+        } catch (URISyntaxException ex) {
+            StringWriter errors = new StringWriter();
+            ex.printStackTrace(new PrintWriter(errors));
+            throw new IOException(errors.toString());
+        }
+    }
 }
--- src/main/java/com/sendgrid/SendGridResponseHandler.java
+++ src/main/java/com/sendgrid/SendGridResponseHandler.java
@@ -7,20 +7,20 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpResponseException;
 import org.apache.http.impl.client.AbstractResponseHandler;
 import org.apache.http.util.EntityUtils;
+
 import java.nio.charset.StandardCharsets;
 
 /**
  * A {@link org.apache.http.client.ResponseHandler} that returns the response body as a String
- * for all responses. 
+ * for all responses.
  * <p>
  * If this is used with
  * {@link org.apache.http.client.HttpClient#execute(
- *  org.apache.http.client.methods.HttpUriRequest, org.apache.http.client.ResponseHandler)},
+ *org.apache.http.client.methods.HttpUriRequest, org.apache.http.client.ResponseHandler)},
  * HttpClient may handle redirects (3xx responses) internally.
  * </p>
- *
  */
-public class SendGridResponseHandler extends AbstractResponseHandler<String>{
+public class SendGridResponseHandler extends AbstractResponseHandler<String> {
 
     /**
      * Read the entity from the response body and pass it to the entity handler
@@ -30,11 +30,11 @@ public class SendGridResponseHandler extends AbstractResponseHandler<String>{
      */
     @Override
     public String handleResponse(final HttpResponse response)
-            throws HttpResponseException, IOException {
+            throws IOException {
         final HttpEntity entity = response.getEntity();
         return entity == null ? null : handleEntity(entity);
     }
-	
+
     @Override
     public String handleEntity(HttpEntity entity) throws IOException {
         return EntityUtils.toString(entity, StandardCharsets.UTF_8);
